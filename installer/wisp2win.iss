b2b19a86e3a37dec05130915d1d89b27774e1790
root <root@knoweat.ru>
2026-05-16 19:40:57 +0000

    Polish recording overlay

--- installer/wisp2win.iss
+++ installer/wisp2win.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Wisp2Win"
-#define MyAppVersion "0.2.2"
+#define MyAppVersion "0.2.3"
 #define MyAppPublisher "knoweat-app"
 #define MyAppExeName "Wisp2Win.exe"
 
--- src/Wisp2Win/Views/RecordingOverlayWindow.xaml.cs
+++ src/Wisp2Win/Views/RecordingOverlayWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 using Wisp2Win.Models;
 using Wisp2Win.ViewModels;
 
@@ -10,12 +12,28 @@ public partial class RecordingOverlayWindow : Window
 {
     private readonly MainViewModel _viewModel;
     private readonly Storyboard _pulseStoryboard;
+    private readonly Storyboard _fadeInStoryboard;
+    private readonly Storyboard _fadeOutStoryboard;
+    private readonly DispatcherTimer _timer;
+    private readonly Stopwatch _stopwatch = new();
+    private bool _isRecordingVisible;
 
     public RecordingOverlayWindow(MainViewModel viewModel)
     {
         InitializeComponent();
         _viewModel = viewModel;
         _pulseStoryboard = (Storyboard)FindResource("PulseStoryboard");
+        _fadeInStoryboard = (Storyboard)FindResource("FadeInStoryboard");
+        _fadeOutStoryboard = (Storyboard)FindResource("FadeOutStoryboard");
+        _fadeOutStoryboard.Completed += (_, _) =>
+        {
+            if (!_isRecordingVisible)
+            {
+                Hide();
+            }
+        };
+        _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+        _timer.Tick += (_, _) => UpdateElapsedText();
         _viewModel.PropertyChanged += OnViewModelPropertyChanged;
         Loaded += (_, _) => PositionWindow();
         SourceInitialized += (_, _) => PositionWindow();
@@ -34,18 +52,57 @@ public partial class RecordingOverlayWindow : Window
     {
         if (_viewModel.State == DictationState.Recording && _viewModel.ShowRecordingOverlay)
         {
-            PositionWindow();
-            if (!IsVisible)
-            {
-                Show();
-            }
-
-            _pulseStoryboard.Begin(this, true);
+            ShowRecording();
             return;
         }
 
+        HideRecording();
+    }
+
+    private void ShowRecording()
+    {
+        if (_isRecordingVisible)
+        {
+            return;
+        }
+
+        _isRecordingVisible = true;
+        _stopwatch.Restart();
+        UpdateElapsedText();
+        PositionWindow();
+        if (!IsVisible)
+        {
+            Opacity = 0;
+            Show();
+        }
+
+        _timer.Start();
+        _fadeOutStoryboard.Stop(this);
+        _fadeInStoryboard.Begin(this, true);
+        _pulseStoryboard.Begin(this, true);
+    }
+
+    private void HideRecording()
+    {
+        if (!_isRecordingVisible && !IsVisible)
+        {
+            return;
+        }
+
+        _isRecordingVisible = false;
+        _stopwatch.Stop();
+        _timer.Stop();
         _pulseStoryboard.Stop(this);
-        Hide();
+        _fadeInStoryboard.Stop(this);
+        _fadeOutStoryboard.Begin(this, true);
+    }
+
+    private void UpdateElapsedText()
+    {
+        var elapsed = _stopwatch.Elapsed;
+        ElapsedText.Text = elapsed.TotalHours >= 1
+            ? elapsed.ToString(@"h\:mm\:ss")
+            : elapsed.ToString(@"mm\:ss");
     }
 
     private void PositionWindow()
@@ -58,6 +115,7 @@ public partial class RecordingOverlayWindow : Window
     protected override void OnClosed(EventArgs e)
     {
         _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
+        _timer.Stop();
         base.OnClosed(e);
     }
 }
